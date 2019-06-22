6264348fe9521fe290616bccd3014de784030e0e
eduardovk <eduardo_vk@hotmail.com>
2019-06-22 18:21:21 -0300

    Added third example and Texts to the scene

---
+++ Assets/Scripts/ClickScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+    public DAS_DialogueSystem dialogueSystem;
+
+    private void OnMouseDown()
+    {
+        dialogueSystem.startDialogue();
+    }
+}
